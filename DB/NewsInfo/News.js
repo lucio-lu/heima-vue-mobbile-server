26e10c2fe2c687e4f65924d37fbace55ee4a6066
lucio-lu <lucio.lu@Outlook.com>
2019-10-22 15:08:29 +0800

    重新处理了news 部分的数据

---
+++ DB/NewsInfo/News.js
@@ -0,0 +1,90 @@
+let News = [
+    {
+        id: 11,
+        title: '彭博社：苹果拟于明年发布全新AR设备及ARM版Mac',
+        add_time: '2019-10-22T08:05:15.000Z',
+        zhaiyao: '彭博社马克·古尔曼（Mark Gurman）近日表示，苹果打算在2020年推出一款全新的增强现实头戴式设备，这款设备将内置有全息显示器。',
+        click: 13,
+        img_url: 'http://cms-bucket.ws.126.net/2019/10/22/0792dd500226432eb8e97987aab6294b.jpeg?imageView&thumbnail=550x0',
+        link: 'https://digi.163.com/19/1022/08/ES30SQ7I001697V8.html'
+    }, {
+        id: 12,
+        title: '马上发布！代工厂已开始生产16英寸苹果MacBook Pro',
+        add_time: '2019-10-22T08:07:07.000Z',
+        zhaiyao: '据外媒最新报道称，广达电脑已经开始为苹果大规模量产16英寸MacBook Pro，而它将在近期发布，当然时间是在10月底。',
+        click: 11,
+        img_url: 'http://cms-bucket.ws.126.net/2019/10/22/77b1188237d348ef8e85e8c01d50db2b.jpeg',
+        link: 'https://digi.163.com/19/1022/08/ES3107MC001697V8.html'
+    }, {
+        id: 13,
+        title: '5G辐射对人体危害大？大佬180度改口',
+        add_time: '2019-10-22T10:43:14.000Z',
+        zhaiyao: '10月21日，在第六届互联网大会乌镇峰会接受采访被问及5G辐射问题时，搜狐公司董事局主席兼首席执行官张朝阳表示：“没问题。”',
+        click: 6,
+        img_url: 'http://cms-bucket.ws.126.net/2019/10/22/b05111a4601946dca6da07af449bfca4.jpeg',
+        link: 'https://digi.163.com/19/1022/10/ES39U2KO001697V8.html'
+    }, {
+        id: 15,
+        title: 'DxOMARK公布谷歌Pixel 4相机得分：总分112/位列第8',
+        add_time: '2019-10-22T08:21:18.000Z',
+        zhaiyao: '近日，DxOMARK官方正式公布了谷歌Pixel 4手机的相机得分，总分为112分，排在第八位；其中拍照得分为117分，视频得分为101分',
+        click: 8,
+        img_url: 'http://cms-bucket.ws.126.net/2019/10/22/6b8c70a61d9b46c8beed068ab7861faa.jpeg',
+        link: 'https://digi.163.com/19/1022/08/ES31Q6I0001697V8.html'
+    }, {
+        id: 16,
+        title: '打破垄断推动开源 指令集RISC-V 芯片市场要变天？',
+        add_time: '2019-10-22T08:19:23.000Z',
+        zhaiyao: '在浙江乌镇第六届世界互联网大会上，中国工程院院士、中国开放指令生态(RISC-V)联盟(CRVA)理事长倪光南发表演讲，希望推动开源的RISC-V成主流，降低芯片的研发成本。',
+        click: 11,
+        img_url: 'http://cms-bucket.ws.126.net/2019/10/22/38bed728a0cd40f899bd2c664246897f.jpeg',
+        link: 'https://digi.163.com/19/1022/08/ES31MLS4001697V8.html'
+    }, {
+        it: 17,
+        title: '共享充电大幅度涨价 集体告别“一元”时代',
+        add_time: '2019-10-22T08:17:02.000Z',
+        zhaiyao: '共享充电宝在发展扩张的过程中，集体告别了“一元”时代，部分租金甚至高达8元/小时，在不同的地区、不同的商铺，共享充电宝都出现了租金不一的收费标准。',
+        click: 9,
+        img_url: 'http://cms-bucket.ws.126.net/2019/10/22/56865a5f060845f3b5afa0301c6813f4.jpeg',
+        link: 'https://digi.163.com/19/1022/08/ES31IC01001697V8.html'
+    }, {
+        id: 18,
+        title: '蔡司ZX1相机有望近期上市：安卓系统/售价近30000元',
+        add_time: '2019-10-22T08:11:58.000Z',
+        zhaiyao: '根据Notebookcheck的报道，蔡司终于要准备发售它在Photokina 2018上发布的ZX1数码相机了。这款相机最初计划在2019年初上市，但到目前为止还没有上市。',
+        click: 6,
+        img_url: 'http://cms-bucket.ws.126.net/2019/10/22/ffc5ea1cc5b84096ac5243f62a83de26.jpeg',
+        link: 'https://digi.163.com/19/1022/08/ES3193J7001697V8.html'
+    }, {
+        id: 19,
+        title: '微软更新补丁中再提示：即将停止对Windows 7支持',
+        add_time: '2019-10-22T08:10:03.000Z',
+        zhaiyao: '在微软刚刚推出的KB4493132累积更新补丁中，他们再一次提醒用户，他们即将停止对Windows 7的支持，所以希望大家赶快升级至Windows 10。',
+        img_url: 'http://cms-bucket.ws.126.net/2019/10/22/7eea9b9414ac4c0dbd1d8e1787c12d45.jpeg',
+        link: 'https://digi.163.com/19/1022/08/ES315IP8001697V8.html'
+    }, {
+        id: 20,
+        title: '谷歌所达成的量子霸权：被IBM拆穿得稀碎',
+        add_time: '2019-10-22T10:41:58.000Z',
+        zhaiyao: '北京时间10月22日消息，IBM公司研究员周一发布论文称，谷歌公司宣称成为了首家实现“量子霸权”的公司，到达了计算历史上的一个转折点，这是错误的。',
+        img_url: 'http://cms-bucket.ws.126.net/2019/10/22/97d228334967427e82d02252581de21c.jpeg',
+        link: 'https://digi.163.com/19/1022/10/ES39ROQ9001697V8.html'
+    }, {
+        id: 21,
+        title: '外媒深扒谷歌Pixel系列“黑历史”：尴尬',
+        add_time: '2019-10-22T09:20:35.000Z',
+        zhaiyao: '10月21日消息，知名科技媒体The Verge发文称谷歌Pixel系列正在不断加入他们之前声称的“所不需要的”功能。',
+        img_url: 'http://cms-bucket.ws.126.net/2019/10/21/03986c37ac4848d0bb650bb2e9adc116.jpeg',
+        link: 'https://digi.163.com/19/1021/09/ES0IQ146001697V8.html'
+    }, {
+        id: 22,
+        title: '技嘉暗示三代撕裂者TRX40新主板：兼容性成谜',
+        add_time: '2019-10-21T09:18:10.000Z',
+        zhaiyao: 'AMD将在11月份发布第三代线程撕裂者，同时还有新的主板芯片组TRX40、TRX80、WRX80，其中后一款面向专业设计领域。',
+        img_url: 'http://cms-bucket.ws.126.net/2019/10/21/8f79af400ded478ca167c851bdb6b1dd.jpeg',
+        ling: 'https://digi.163.com/19/1021/09/ES0ILJD2001697V8.html'
+    }
+]
+
+
+module.exports.News = News
--- Public/Controller/NewsController.js
+++ Public/Controller/NewsController.js
@@ -1,146 +1,57 @@
 const fs = require('fs')
+const db = require(`../../db/NewsInfo/News.js`)
+const path = require('path')
 
-let message = [
-    {
-        id: 11,
-        title: '配送交付时间轻量级预估实践',
-        add_time: '2019-10-14T11:24:04.000Z',
-        zhaiyao: '在本文中，我们介绍了交付时间预估迭代的三个版本，分别为基于地址结构的树模型、向量召回方案以及轻量级的End-to-End的深度学习网络',
-        click: 3,
-        link: 'https://www.cnblogs.com/meituantech/p/11670517.html',
-        img_url: 'https://avatars1.githubusercontent.com/u/13275792?s=40&v=4'
-    },
-    {
-        id: 12,
-        title: 'spring5 源码深度解析----- Spring事务 是怎么通过AOP实现的？（100%理解Spring事务）',
-        add_time: '2019-10-12T20:03:52.000Z',
-        zhaiyao: '此篇文章需要有SpringAOP基础，知道AOP底层原理可以更好的理解Spring的事务处理。 自定义标签 对于Spring中事务功能的代码分析',
-        click: 2,
-        link: 'https://www.cnblogs.com/java-chen-hao/p/11635380.html',
-        img_url: 'https://avatars1.githubusercontent.com/u/13275792?s=40&v=4'
-    },
-    {
-        id: 13,
-        title: '.NET Core 3.0之深入源码理解ObjectPool（一）',
-        add_time: '2019-10-10T23:12:42.000Z',
-        zhaiyao: '写在前面 对象池是一种比较常用的提高系统性能的软件设计模式，它维护了一系列相关对象列表的容器对象，这些对象可以随时重复使用',
-        click: 5,
-        link: 'https://www.cnblogs.com/edison0621/p/11669000.html',
-        img_url: 'https://avatars1.githubusercontent.com/u/13275792?s=40&v=4'
-    },
-    {
-        id: 14,
-        title: '多线程基础体系知识清单',
-        add_time: '2019-10-7T08:53:02.000Z',
-        zhaiyao: '前言 本文会介绍Java中多线程与并发的基础，适合初学者食用。 线程与进程的区别 在计算机发展初期，每台计算机是串行地执行任务的，如果碰上需要IO的地方',
-        click: 3,
-        link: 'https://www.cnblogs.com/javazhiyin/p/11670356.html',
-        img_url: 'https://avatars1.githubusercontent.com/u/13275792?s=40&v=4'
-    },
-    {
-        id: 15,
-        title: 'BeetleX服务网关之限流和缓存',
-        add_time: '2019-10-7T11:07:13.000Z',
-        zhaiyao: '限流和缓存是网关中两个非常重要的功能，前者是保障服务更可靠地运行，后者则可以大大提高应用的吞吐能力',
-        click: 1,
-        link: 'https://www.cnblogs.com/smark/p/11670324.html',
-        img_url: 'https://avatars1.githubusercontent.com/u/13275792?s=40&v=4'
-    },
-    {
-        id: 16,
-        title: '原生js实现Promise',
-        add_time: '2019-10-6T20:17:24.000Z',
-        zhaiyao: '由于浏览器兼容性的限制，我们不得不通过原生js实现Promise方法。 原生的Promise对象包含promise,promiseAll,rase等方法',
-        click: 10,
-        link: 'https://www.cnblogs.com/shizhouyu/p/11670124.html',
-        img_url: 'https://avatars1.githubusercontent.com/u/13275792?s=40&v=4'
-    },
-    {
-        id: 17,
-        title: 'Vue躬行记（2）——指令',
-        add_time: '2019-10-3T12:24:37.000Z',
-        zhaiyao: 'Vue不仅内置了各类指令，包括条件渲染、事件处理等，还能注册自定义指令。 一、条件渲染 条件渲染的指令包括v-if、v-else、v-else-if和v-show',
-        click: 2,
-        link: 'https://www.cnblogs.com/strick/p/11298122.html',
-        img_url: 'https://avatars1.githubusercontent.com/u/13275792?s=40&v=4'
-    },
-    {
-        id: 18,
-        title: '手把手带你体验Stream流',
-        add_time: '2019-10-3T08:38:13.000Z',
-        zhaiyao: '前言 只有光头才能变强。 文本已收录至我的GitHub仓库，欢迎Star： "https://github.com/ZhongFuCheng3y/3y"',
-        click: 3,
-        link: 'https://www.cnblogs.com/Java3y/p/11669727.html',
-        img_url: 'https://avatars1.githubusercontent.com/u/13275792?s=40&v=4'
-    },
-    {
-        id: 19,
-        title: '你也可以写个聊天程序 - C# Socket学习1',
-        add_time: '2019-10-2T18:38:42.000Z',
-        zhaiyao: '简述 我们做软件工作的虽然每天都离不开网络，可网络协议细节却不是每个人都会接触和深入了解。我今天就来和大家一起学习下Socket，并写一个简单的聊天程序',
-        click: 5,
-        link: 'https://www.cnblogs.com/zhaopei/p/Socket1.html',
-        img_url: 'https://avatars1.githubusercontent.com/u/13275792?s=40&v=4'
-    },
-    {
-        id: 20,
-        title: '程序员修神之路--设计一套RPC框架并非易事',
-        add_time: '2019-10-2T15:23:09.000Z',
-        zhaiyao: '我最近终于把Socket通信调通了 这么底层的东西你现在都会了，恭喜你离涨薪又进一步呀 http协议不也是利用的Socket吗 可以这么说，http协议是基于TCP协议的',
-        click: 2,
-        link: 'https://www.cnblogs.com/zhanlang/p/11664221.html',
-        img_url: 'https://avatars1.githubusercontent.com/u/13275792?s=40&v=4'
-    },
-    {
-        id: 21,
-        title: 'Fcitx使用搜狗词库与皮肤',
-        add_time: '2019-10-2T09:11.13.000Z',
-        zhaiyao: '在 $\text{Linux}$ 环境下，$\text{Fcitx}$ 确实是最好用的开源输入法之一。然而 $\text{Windows}$ 下的巨头输入法 —— 搜狗，对 $\text{Linux}$ 的支持却并不算到位',
-        click: 1,
-        link: 'https://www.cnblogs.com/luoshuitianyi/p/11669619.html',
-        img_url: 'https://avatars1.githubusercontent.com/u/13275792?s=40&v=4'
-    },
-    {
-        id: 22,
-        title: '通俗易懂设计模式解析——状态模式',
-        add_time: '2019-10-1T23:03:24.000Z',
-        zhaiyao: '前言 今天我们讲的是状态模式【State Pattern】、这个名字咋一看不好理解，但是仔细一想还是比较容易的。状态模式重点关注的是状态。状态又牵扯着什么呢',
-        click: 1,
-        link: 'https://www.cnblogs.com/hulizhong/p/11662201.html',
-        img_url: 'https://avatars1.githubusercontent.com/u/13275792?s=40&v=4'
-    },
-    {
-        id: 23,
-        title: '.NET斗鱼直播弹幕客户端(下)',
-        add_time: '2019-10-1T15:03:57.000Z',
-        zhaiyao: '在上篇文章中，我们提到了如何使用.NET连接斗鱼TV直播弹幕的基本操作。然而想要做得好，做得容易扩展，就需要做进一步的代码整理',
-        click: 1,
-        link: 'https://www.cnblogs.com/sdflysha/p/20191013-douyu-barrage-with-dotnet-2.html',
-        img_url: 'https://avatars1.githubusercontent.com/u/13275792?s=40&v=4'
-    },
-]
 
 let getNewsList = function () {
-    let result = { status: 0, message: message }
+    let result = { status: 0, message: db.News }
     return result
 }
 
 let getNewsInfo = function (_id) {
     let result = { status: -1, message: null }
 
-    var _index = message.findIndex(p => p.id == _id)
+    var _index = db.News.findIndex(p => p.id == _id)
     if (_index > -1) {
-        var msg = message[_index]
+        var msg = db.News[_index]
 
-        let dataStr = fs.readFileSync(`Public/NewsInfo/${msg.id}.txt`, 'UTF-8')//@@@这里为什么要从public开始，而不是../
-        var msginfo = [{
-            id: msg.id,
-            title: msg.title,
-            click: msg.click,
-            add_time: msg.add_time,
-            content: dataStr
-        }] // 这个接口返回的是数组
-        result = { status: 0, message: msginfo }
+        // C:\
+        //     Documents\
+        //         Heima\
+        //             heima-vue-mobbile-server\
+        //                 app.js                                   -> 启动文件
+        //                 Public\
+        //                     Controller\                          -> 当前目录
+        //                         NewsControler.js                 -> 当前文件        (input)
+        //                 DB\
+        //                     NewsInfo\
+        //                         11.html                          -> 我要读取的文件   (output)
+        // @@@ 不知道为什么，它是从启动文件开始算起的，而不是当前文件的相对路径
+        // __dirname 和 __filename 不是全局对象，参考：https://nodejs.org/dist/latest-v12.x/docs/api/modules.html#modules_dirname
+        // console.log(__dirname);                                  // ->  C:\Documents\Heima\heima-vue-mobbile-server\Public\Controller
+        // console.log(__filename);                                 // ->  C:\Documents\Heima\heima-vue-mobbile-server\Public\Controller\NewsController.js
+        // @@@ 我短时间内并没有找到为什么 node 计算相对路径的时候，不是按照当前路径去计算的，所以想要计算相对路径，需要使用__dirname和Path对象
+        // https://nodejs.org/dist/latest-v12.x/docs/api/path.html
+        // path.join(__dirname, '../../db/NewsInfo/11.html')        //->  { ... dir: 'C:\Documents\Heima\heima-vue-mobbile-server\db\NewsInfo\11.html', ...}
+
+        let _newsContentPath = path.join(__dirname, `../../db/NewsInfo/${msg.id}.html`)
+
+        let exists = fs.existsSync(_newsContentPath)
+        if (exists) {
+            let dataStr = fs.readFileSync(_newsContentPath, 'UTF-8')//@@@这里为什么要从public开始，而不是../
+            var msginfo = [{
+                id: msg.id,
+                title: msg.title,
+                click: msg.click,
+                add_time: msg.add_time,
+                content: dataStr
+            }] // 这个接口返回的是数组
+            result = { status: 0, message: msginfo }
+        } else {
+            let errMsg = 'Error!!! 没有找到文件 id: ' + msg.id
+            console.log(errMsg)
+            result = { status: -1, message: errMsg }
+        }
     }
     return result
 }
